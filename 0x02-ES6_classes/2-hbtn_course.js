b4544d082ee30c98bce0191fa95d3d8ba3abf335
“abdullah <abdullah.h.elgabry@gmail.com>
2024-07-26 20:52:16 +0300

    Task solved

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,3 +1,10 @@
+/**
+ * Implement a class named HolbertonCourse
+ * Constructor attributes:
+ * name (String)
+ * length (Number)
+ * students (array of Strings)
+ */
 export default class HolbertonCourse {
 
   constructor(name, length, students) {
@@ -6,10 +13,16 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
+  /**
+   * course Name
+   */
   get name() {
     return this._name;
   }
 
+  /**
+   * set course name.
+   */
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -17,11 +30,16 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
-
+  /**
+   * COURSE getter
+   */
   get length() {
     return this._length;
   }
 
+  /**
+   * COURSE setter
+   */
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -29,11 +47,16 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
-
+  /**
+   * students getter
+   */
   get students() {
     return this._students;
   }
 
+  /**
+   * students setter
+   */
   set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');

1144567c3b881b30f99c932f0823d50f063b1e4f
“abdullah <abdullah.h.elgabry@gmail.com>
2024-07-30 13:17:31 +0300

    task #Done

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+// getStudentsByLocation() that returns an array of objects
+// who are located in a specific city.
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}

0d8a940073551877d33cf25c6018b931496b8c4c
“abdullah <abdullah.h.elgabry@gmail.com>
2024-07-30 13:29:20 +0300

    task #Done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,3 @@
-// updateStudentGradeByCity that returns an array of students
-// for a specific city with their new grade.
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const defaultGrade = { grade: 'N/A' };
 
